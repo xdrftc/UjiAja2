151bfa8f594da5b4d195244eb8060469b5f05699
xdrftc <daffaexe959@gmail.com>
2025-11-27 13:28:50 +0700

    initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -28,6 +30,11 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+
+    dependencies {
+        implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+        implementation("com.google.firebase:firebase-analytics")
     }
 
     buildTypes {
